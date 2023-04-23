d1fbc9ec02194dcb45147723a34c57514db15188
Eunseong Jang <ejang21@umd.edu>
2023-04-23 12:05:28 -0400

    check if there are lost data after renaming county

---
+++ drug counts check.R
@@ -0,0 +1,48 @@
+library(readxl)
+library(urbnmapr)
+library(tidyverse)
+library(gridExtra)
+library(lubridate)
+
+# urbnmapr data
+unique(countydata$county_fips) # some ips have 5 digits. need to be converted into integer.
+HIDTA.dist <- read.csv("HIDTA Regions.csv", header=T) %>% as_tibble
+coordinate.HIDTA <- left_join(counties, HIDTA.dist, by=c("state_name", "county_name")) %>% filter(!(state_name %in% c("Alaska", "Hawaii", "Puerto Rico")))
+names(coordinate.HIDTA)[7] <- "GEOID"
+coordinate.HIDTA$county <- str_split(coordinate.HIDTA$county_name, " ") %>%
+  lapply(function(x) str_c(c(x[(1:(length(x)-1))]), collapse=" ") ) %>%
+  unlist
+HIDTA.regions <- coordinate.HIDTA %>% filter(!is.na(HIDTA)) %>% select(GEOID, HIDTA) %>% unique
+
+seizures <- read_xlsx("Drug Seizures All HIDTAs All Drugs 2018-2021 Original.xlsx") %>% filter(State %in% coordinate.HIDTA$state_name)
+seizures <- seizures %>% filter(County != "Michigan")
+seizures$County <- substring(seizures$County, 1, str_locate(seizures$County, ",")[,1]-1)
+seizures$Year <- substring(seizures$SeizureDate, 1, 4)
+seizures$Month <- substring(seizures$SeizureDate, 6, 7)
+seizures$Year <- as.numeric(seizures$Year)
+seizures$Month <- as.numeric(seizures$Month)
+names(seizures)[1:2] <- c("state_name", "county")
+all_counts <- seizures %>% 
+  group_by(Drug) %>%
+  summarise(count=sum(Quantity > 0)) %>% 
+  arrange(desc(count))
+all_counts
+seizures %>% 
+  group_by(Drug) %>%
+  summarise(count=n()) %>% 
+  arrange(desc(count))
+
+seizures_renamed <- read_xlsx("Drug Seizures All HIDTAs All Drugs 2018-2021 Combined.xlsx") %>% filter(State %in% coordinate.HIDTA$state_name)
+seizures_renamed <- seizures_renamed %>% filter(County != "Michigan")
+seizures_renamed$County <- substring(seizures_renamed$County, 1, str_locate(seizures_renamed$County, ",")[,1]-1)
+seizures_renamed$Year <- substring(seizures_renamed$SeizureDate, 1, 4)
+seizures_renamed$Month <- substring(seizures_renamed$SeizureDate, 6, 7)
+seizures_renamed$Year <- as.numeric(seizures_renamed$Year)
+seizures_renamed$Month <- as.numeric(seizures_renamed$Month)
+names(seizures_renamed)[1:2] <- c("state_name", "county")
+all_counts_renamed <- seizures_renamed %>% 
+  group_by(Drug) %>%
+  summarise(count=sum(Quantity > 0)) %>% 
+  arrange(desc(count))
+
+all_counts
